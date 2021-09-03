24e0dedc3c6553fe18a982e570ef91593e224f3b
Willy1999927 <48126548+Willy1999927@users.noreply.github.com>
2021-09-03 15:07:32 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,33 @@ from flask import Flask,request,jsonify
 from flask_cors import CORS
 from PIL import Image
 from io import BytesIO 
-import model_set as m
 
 import time
 import cv2 as cv
 import numpy as np
 app=Flask(__name__)
 CORS(app)
-    
+
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
 @app.route('/predict',methods=['POST'])
 def predict():
     faceProto = "model/opencv_face_detector.pbtxt"
@@ -42,7 +61,7 @@ def predict():
             data['predictions'] = []
             # Read frame
             t = time.time()
-            frameFace, bboxes = m.getFaceBox(faceNet, image)
+            frameFace, bboxes = getFaceBox(faceNet, image)
             if not bboxes:
                 print("No face Detected, Checking next frame")
 
