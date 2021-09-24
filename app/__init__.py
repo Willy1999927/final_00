f644af567926c370b2c83353140db05dd0c5303e
Willy1999927 <48126548+Willy1999927@users.noreply.github.com>
2021-09-24 17:56:07 +0800

    Update __init__.py
    version_1.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,26 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
-from PIL import Image
 from io import BytesIO 
 
+import PIL.Image
 import time
 import cv2 as cv
 import numpy as np
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app=Flask(__name__)
 CORS(app)
+
+def load_image_file(file, mode='RGB'):
+    im = PIL.Image.open(file)
+    if mode:
+        im = im.convert(mode)
+    return np.array(im)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def getFaceBox(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
@@ -29,16 +42,45 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
     return frameOpencvDnn, bboxes
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    faceProto = "app/model/opencv_face_detector.pbtxt"
-    faceModel = "app/model/opencv_face_detector_uint8.pb"
 
-    ageProto = "app/model/age_deploy.prototxt"
-    ageModel = "app/model/age_net.caffemodel"
+@app.route('/predict', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
 
-    genderProto = "app/model/gender_deploy.prototxt"
-    genderModel = "app/model/gender_net.caffemodel"
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            print(file)
+            # The image file seems valid! Detect faces and return the result.
+            return predict(file)
+
+    # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see age and gender</h1>
+    <form method="POST" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Upload">
+    </form>
+    '''
+
+
+def predict(file_stream):
+    faceProto = "model/opencv_face_detector.pbtxt"
+    faceModel = "model/opencv_face_detector_uint8.pb"
+
+    ageProto = "model/age_deploy.prototxt"
+    ageModel = "model/age_net.caffemodel"
+
+    genderProto = "model/gender_deploy.prototxt"
+    genderModel = "model/gender_net.caffemodel"
     MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
     ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
     genderList = ['Male', 'Female']
@@ -46,60 +88,47 @@ def predict():
     ageNet = cv.dnn.readNet(ageModel, ageProto)
     genderNet = cv.dnn.readNet(genderModel, genderProto)
     faceNet = cv.dnn.readNet(faceModel, faceProto)
+    
     # initialize the data dictionary that will be returned from the
     # view
     data = {'success': False}
     print('request')
     # ensure an image was properly uploaded to our endpoint
-    if request.method == 'POST':
-        if request.files.get('image'):
-            # 從 flask request 中讀取圖片（byte str）
-            image = request.files['image'].read()
-            image = cv.imdecode(np.frombuffer(image, np.uint8), cv.IMREAD_COLOR)
+    # 從 flask request 中讀取圖片（byte str）
+    image = load_image_file(file_stream)
+    print(file_stream)
 
-            padding = 20
-            data['predictions'] = []
-            # Read frame
-            t = time.time()
-            frameFace, bboxes = getFaceBox(faceNet, image)
-            if not bboxes:
-                print("No face Detected, Checking next frame")
+    padding = 20
+    data['predictions'] = []
+    # Read frame
+    t = time.time()
+    frameFace, bboxes = getFaceBox(faceNet, image)
+    if not bboxes:
+        print("No face Detected, Checking next frame")
 
-            for bbox in bboxes:
-                face = image[max(0,bbox[1]-padding):min(bbox[3]+padding,image.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, image.shape[1]-1)]
+    for bbox in bboxes:
+        face = image[max(0,bbox[1]-padding):min(bbox[3]+padding,image.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, image.shape[1]-1)]
 
-                blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-                genderNet.setInput(blob)
-                genderPreds = genderNet.forward()
-                gender = genderList[genderPreds[0].argmax()]
-                # print("Gender Output : {}".format(genderPreds))
-                #print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        # print("Gender Output : {}".format(genderPreds))
+        #print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
                     
-                ageNet.setInput(blob)
-                agePreds = ageNet.forward()
-                age = ageList[agePreds[0].argmax()]
-                #print("Age Output : {}".format(agePreds))
-                #print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
-                r = {'number' : bbox,'gender': gender, 'age': age}
-                data['predictions'].append(r)
-            data['success'] = True
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        #print("Age Output : {}".format(agePreds))
+        #print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+        r = {'number' : bbox,'gender': gender, 'age': age}
+        data['predictions'].append(r)
+    data['success'] = True
+    label = "{},{}".format(gender, age)
+    cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+    cv.imwrite('static/picture/output.jpg', frameFace)
 
     return jsonify(data)
-
-@app.route('/getphoto')
-def getphoto():
-    return 'hi getphoto!'
-
-@app.route('/img/send_img', methods=['POST'])
-def send_img():
-    f = request.files['file']
-    img = f.read()
-    print('img',type(img))
-    im1 = Image.open(img)
-    print('im1',type(im1))
-    im2 = cv.imread(img)
-    print('im2',type(im2))
-    return 'Send Img Test'
 
 if __name__ == '__main__':
     print(('* Loading Keras model and Flask starting server...'
